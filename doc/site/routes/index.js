8d68618be81b5288848997dcf37f400401e58f61
MGCSousa <manuelgcsousa@gmail.com>
2020-06-13 13:44:11 +0100

    added download functionality (tool.zip test file)

--- doc/site/routes/index.js
+++ doc/site/routes/index.js
@@ -1,5 +1,6 @@
 var express    = require('express');
 var router     = express.Router();
+const path     = require('path');
 const jsonfile = require('jsonfile');
 
 const feedback = (__dirname + '/../data/feedback.json');
@@ -8,6 +9,11 @@ const feedback = (__dirname + '/../data/feedback.json');
 // home page
 router.get('/', function(req, res, next) {
     res.render('home', { title: 'Home' });
+});
+
+// download tool
+router.get('/download', function(req, res, next) {
+    res.download(path.join(__dirname, '../_download/tool.zip'));
 });
 
 // tool page
@@ -25,6 +31,7 @@ router.get('/feedback', (req, res, next) => {
     res.render('feedback', { title: 'feedback' });
 });
 
+// process new feedback
 router.post('/feedback', (req, res, next) => {
     const res_feedback = JSON.parse(JSON.stringify(req.body));
     res_feedback['data'] = new Date();
